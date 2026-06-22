7c6fe3271ba18a713d03a2ec83d8d335a02b08f3
briangabrielfsoares-star <briangabrielfsoares@gmail.com>
2026-06-22 18:30:27 -0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,53 @@
-console.log("Empire Imports");
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Checkout - Empire Imports</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+<h1>EMPIRE IMPORTS</h1>
+</header>
+
+<section class="hero">
+<h2>FINALIZAR COMPRA</h2>
+<p>Preencha os dados abaixo.</p>
+</section>
+
+<section class="produtos">
+
+<div class="produto">
+
+<input type="text" placeholder="Nome Completo"><br><br>
+
+<input type="text" placeholder="CPF"><br><br>
+
+<input type="text" placeholder="Telefone"><br><br>
+
+<input type="text" placeholder="Endereço"><br><br>
+
+<input type="text" placeholder="Cidade"><br><br>
+
+<input type="text" placeholder="CEP"><br><br>
+
+<h3>Forma de Pagamento</h3>
+
+<button>PIX</button>
+
+<button>Cartão de Crédito</button>
+
+<button>Mercado Pago</button>
+
+<h2>Total: R$489,80</h2>
+
+<button>Confirmar Pedido</button>
+
+</div>
+
+</section>
+
+</body>
+</html>
