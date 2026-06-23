3f61cc8ec9984516e1c37f9673c1a94faf01e57a
briangabrielfsoares-star <briangabrielfsoares@gmail.com>
2026-06-23 18:35:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
-let cart = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+updateCart();
 
 function addToCart(product) {
   cart.push(product);
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
   updateCart();
 }
 
 function updateCart() {
+
   document.getElementById("cartCount").innerText = cart.length;
 
   let list = document.getElementById("cartList");
+
   list.innerHTML = "";
 
   cart.forEach(item => {
+
     let li = document.createElement("li");
+
     li.textContent = item;
+
     list.appendChild(li);
+
   });
+
 }
 
 function finalizarCompra() {
+
   if (cart.length === 0) {
+
     alert("Carrinho vazio!");
+
     return;
+
   }
 
-  let mensagem = "Olá, quero comprar:%0A" + cart.join("%0A");
-  window.open("https://wa.me/5531972354299?text=" + mensagem);
+  let mensagem = "Olá, quero comprar:%0A";
+
+  cart.forEach(item => {
+
+    mensagem += "- " + item + "%0A";
+
+  });
+
+  window.open(
+    "https://wa.me/5531972354299?text=" + mensagem,
+    "_blank"
+  );
+
 }
