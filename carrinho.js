db69908b429c7c42a40cf9e929270aa510839b53
briangabrielfsoares-star <briangabrielfsoares@gmail.com>
2026-06-23 14:33:11 -0300

    Update carrinho.js

--- carrinho.js
+++ carrinho.js
@@ -1,33 +1,92 @@
-let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+let carrinho =
+JSON.parse(localStorage.getItem("carrinho")) || [];
 
-let lista = document.getElementById("lista-carrinho");
+let lista =
+document.getElementById("lista-carrinho");
+
 let total = 0;
 
-lista.innerHTML = "";
+if(carrinho.length == 0){
 
-carrinho.forEach((produto, index) => {
+    lista.innerHTML = `
 
-    total += produto.preco;
-
-    lista.innerHTML += `
     <div class="produto">
-        <h3>${produto.nome}</h3>
-        <h2>R$${produto.preco.toFixed(2)}</h2>
-        <button onclick="removerProduto(${index})">
-            Remover
-        </button>
+
+        <h2>
+
+        Seu carrinho está vazio 🛒
+
+        </h2>
+
+        <p>
+
+        Adicione produtos para continuar.
+
+        </p>
+
     </div>
+
     `;
-});
+
+}
+
+else{
+
+    lista.innerHTML = "";
+
+    carrinho.forEach((produto,index)=>{
+
+        total += produto.preco;
+
+        lista.innerHTML += `
+
+        <div class="produto">
+
+            <h3>
+
+            ${produto.nome}
+
+            </h3>
+
+            <h2>
+
+            R$${produto.preco.toFixed(2)}
+
+            </h2>
+
+            <br>
+
+            <button
+            onclick="removerProduto(${index})">
+
+            Remover
+
+            </button>
+
+        </div>
+
+        `;
+
+    });
+
+}
 
 document.getElementById("total").innerHTML =
+
 `Total: R$${total.toFixed(2)}`;
 
-function removerProduto(index) {
+function removerProduto(index){
 
-    carrinho.splice(index, 1);
+    carrinho.splice(index,1);
 
-    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+    localStorage.setItem(
+
+    "carrinho",
+
+    JSON.stringify(carrinho)
+
+    );
 
     location.reload();
+
 }
