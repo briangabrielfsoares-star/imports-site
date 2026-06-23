72e80fa5bb170d284fb7adcd79ac427c01bade76
briangabrielfsoares-star <briangabrielfsoares@gmail.com>
2026-06-23 18:41:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,31 @@ function finalizarCompra() {
   );
 
 }
+function searchProducts() {
+
+  let input = document
+    .getElementById("searchInput")
+    .value
+    .toLowerCase();
+
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+
+    let title = card.querySelector("h3")
+      .innerText
+      .toLowerCase();
+
+    if (title.includes(input)) {
+
+      card.style.display = "block";
+
+    } else {
+
+      card.style.display = "none";
+
+    }
+
+  });
+
+}
