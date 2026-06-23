7723b3af42b0554570442808c6b48c8a1a0386d0
briangabrielfsoares-star <briangabrielfsoares@gmail.com>
2026-06-23 13:49:58 -0300

    Update carrinho.js

--- carrinho.js
+++ carrinho.js
@@ -1,1 +1,16 @@
-console.log("Empire Imports");
+let carrinho = [];
+
+function adicionarAoCarrinho(nome, preco) {
+
+    carrinho.push({
+        nome: nome,
+        preco: preco
+    });
+
+    localStorage.setItem(
+        "carrinho",
+        JSON.stringify(carrinho)
+    );
+
+    alert(nome + " foi adicionado ao carrinho!");
+}
