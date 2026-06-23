c8a4b1815826b6158f43a213eda9c401dab2365e
briangabrielfsoares-star <briangabrielfsoares@gmail.com>
2026-06-23 13:56:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Empire Imports carregada com sucesso!");
+let botoes = document.querySelectorAll(".adicionar-carrinho");
+
+botoes.forEach(botao => {
+
+    botao.addEventListener("click", () => {
+
+        let nome = botao.dataset.nome;
+        let preco = Number(botao.dataset.preco);
+
+        let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+        carrinho.push({
+            nome: nome,
+            preco: preco
+        });
+
+        localStorage.setItem("carrinho", JSON.stringify(carrinho));
+
+        alert("Produto adicionado ao carrinho!");
+    });
+
+});
